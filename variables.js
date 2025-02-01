22ca23cb999b85dd82e9734f8794127864ee5d3e
dogfriendbookclub <z.b.slavin@gmail.com>
2025-02-01 12:59:07 -0600

    fix date again

--- variables.js
+++ variables.js
@@ -1,3 +1,3 @@
-export const voteStart = new Date("2025-02-01");
+export const voteStart = new Date("2025-01-31");
 export const voteEnd = new Date("2025-02-09");
 export const meetingTime = new Date("Feb 1, 2025 14:30:00");

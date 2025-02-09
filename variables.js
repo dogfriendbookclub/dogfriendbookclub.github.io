4b5324443f12cb3427552183f9d63f1bbcbf619f
dogfriendbookclub <z.b.slavin@gmail.com>
2025-02-09 15:04:56 -0600

    updates so variables work

--- variables.js
+++ variables.js
@@ -4,7 +4,17 @@ export const meetingTime = new Date("Mar 1, 2025 13:00:00");
 export const currentFormID = "250315106507143"
 export const resultsReportID = "25039347178906509"
 export const meetingPlace = "Fox and Pantry in Plymouth"
-export const meetingPlaceIframe = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2820.0742405849032!2d-93.48369862237185!3d45.023418371070356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b3495a1acf9fe1%3A0x22face9b19971a64!2sThe%20Fox%20and%20Pantry!5e0!3m2!1sen!2sus!4v1739131845662!5m2!1sen!2sus\" width=\"100%\" height=\"100%\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>"
+export const meetingPlaceIframe = `
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2820.0742405849032!2d-93.48369862237185!3d45.023418371070356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b3495a1acf9fe1%3A0x22face9b19971a64!2sThe%20Fox%20and%20Pantry!5e0!3m2!1sen!2sus!4v1739131845662!5m2!1sen!2sus"
+    width="100%"
+    height="100%"
+    style="border:0;"
+    allowfullscreen=""
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade">
+  </iframe>
+`;
 export const meetingDataID = "MG24832380"
 export const meetingName = "Short Story Meeting"
 console.log("meeting place is", meetingPlace)

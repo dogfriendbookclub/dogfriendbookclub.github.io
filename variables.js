ec91023d6838f13082771152f32df5ed08361cfb
dogfriendbookclub <z.b.slavin@gmail.com>
2025-02-09 10:26:04 -0600

    fix export variable

--- variables.js
+++ variables.js
@@ -2,5 +2,6 @@ export const voteStart = new Date("2025-01-31");
 export const voteEnd = new Date("2025-02-09");
 export const meetingTime = new Date("Feb 1, 2025 14:30:00");
 export const currentFormID = 250315106507143
+export const resultsReportID = 25039347178906509
 window.resultsReportID = 25039347178906509; // Make it accessible globally
 console.log("resultsReportID from variables.js:", window.resultsReportID);

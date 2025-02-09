f870ea61794b633408e0c5fe17c73ba2280c8b85
dogfriendbookclub <z.b.slavin@gmail.com>
2025-02-09 10:28:21 -0600

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,5 +3,4 @@ export const voteEnd = new Date("2025-02-09");
 export const meetingTime = new Date("Feb 1, 2025 14:30:00");
 export const currentFormID = 250315106507143
 export const resultsReportID = 25039347178906509
-window.resultsReportID = 25039347178906509; // Make it accessible globally
 console.log("resultsReportID from variables.js:", window.resultsReportID);
